fa78d2d1e963b9fdade72e519fdd792b60becbb6
Benjamin Englard <benjienglard@yahoo.com>
2015-09-29 10:00:11 -0700

    async now does multipart form parsing

--- waffle/request.lua
+++ waffle/request.lua
@@ -32,69 +32,27 @@ end
 local _getform = function(self)
    self.form = {}
    if self.body ~= '' then
-      local idx = 0
       local rbody = self.body
-      local lrb = #rbody
-
-      while idx < lrb do
+      if type(rbody) == 'table' then
          -- Multipart form encoded
-         local inner = string.find(rbody, 'Content-', idx)
-         if inner == nil then break end
-         local rest = rbody:sub(inner)
-         idx = inner + 1
-
-         inner = string.find(rbody, '\r', inner)
-         rest = rbody:sub(idx, inner - 1):gsub('"', '')
-         
-         local data = string.split(rest, ';')
-         local nd = #data
-         local isfile = nd > 2
-         local name = ''
-
-         if isfile then
-            name = data[2]:gsub(' ', ''):split('=')[2]
-            local fname = data[3]:gsub(' ', ''):split('=')[2]
-
-            -- Find content-type
-            idx = inner
-            inner = string.find(rbody, '\r', idx + 1)
-            rest = rbody:sub(idx, inner - 1)
-            local ctype = rest:split(':')[2]:gsub(' ', '')
-            local binary = ctype:find('text') == nil
-
-            self.form[name] = {
-               filename = fname,
-               type = ctype,
-               binary = binary
-            }
-
-            idx = inner
-         else
-            name = data[2]:gsub(' ', ''):split('=')[2]
-            self.form[name] = ''
+         for key, value in pairs(rbody) do
+            local notfile = value.filename == nil
+            if notfile then
+               self.form[key] = value.data
+            else
+               local ctype = value['content-type']
+               local binary = ctype:find('text') == nil
+               self.form[key] = {
+                  data = value.data,
+                  type = ctype,
+                  filename = value.filename,
+                  binary = binary,
+                  save = _save
+               }
+            end
          end
-
-         inner = inner + 4
-
-         local inner2 = string.find(rbody, 'FormBoundary', inner) - 1
-         while inner2 > 1 do
-            inner2 = inner2 - 1
-            local c = rbody:sub(inner2, inner2 + 1)
-            if string.byte(c) == 13 then break end
-         end
-         rest = rbody:sub(inner, inner2 - 1)
-
-         if isfile then
-            self.form[name].save = _save
-            self.form[name].data = rest
-         else
-            self.form[name] = rest
-         end
-
-         idx = inner2
-      end
-
-      if idx == 0 then -- URL encoded
+      else
+         -- URL Encoded
          for param in string.gsplit(rbody, '&') do
             local arg = string.split(param, '=')
             self.form[arg[1]] = encodings.urldecode(arg[2])
